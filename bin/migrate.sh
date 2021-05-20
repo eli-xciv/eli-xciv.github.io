2dde6e16e87bc551708fbdbe58e7e1acb7c2abac
Eli <eli-xciv@protonmail.com>
2021-05-20 11:29:29 -0400

    Blog Update: 2021-05-20 11:29:15

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+printf -v date '%(%Y-%m-%d %H:%M:%S)T\n' -1
+
 gssg ---domain "http://localhost:8080" --url "https://eli-xciv.github.io" --dest docs
+
+git add .
+
+git commit -m "Blog Update: $date"
+
+git push
